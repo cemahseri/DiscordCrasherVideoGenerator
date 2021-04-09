a6f9827ab7e7d2548f28c5acbcc284fefba2493e
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-04-09 18:43:49 +0200

    Mmmh

--- Program.cs
+++ Program.cs
@@ -11,9 +11,7 @@ namespace DiscordCrasherVideoGenerator
         {
             if (args.Length != 1 && args.Length != 2)
             {
-                Console.WriteLine("Usage #1: Drag-and-drop video to DiscordCrasherVideoGenerator   (Output file name: OriginalFileName_crasher.mp4)"); // See comment line below.
-                Console.WriteLine("Usage #2: DiscordCrasherVideoGenerator input.mp4                (Output file name: input_crasher.mp4)");
-                Console.WriteLine("Usage #3: DiscordCrasherVideoGenerator input.mp4 output.mp4");
+                Console.WriteLine("Usage #1: Drag-and-drop video to DiscordCrasherVideoGenerator   (Output file name: OriginalFileName_crasher.mp4)\nUsage #2: DiscordCrasherVideoGenerator input.mp4                (Output file name: input_crasher.mp4)\nUsage #3: DiscordCrasherVideoGenerator input.mp4 output.mp4");
                 return;
             }
 
@@ -24,8 +22,7 @@ namespace DiscordCrasherVideoGenerator
             //   due to differences in ratio aspects and resolutions (and also corrupting the crasher data), we are going to use concat demuxer (-f concat).
             // Sadly, this way do not support passing file names as parameters. Well, FFmpeg has built in query executor but it won't work on every platform.
             // So, we are going to create a temporary file called "files", write the file we are going to make crasher and our crasher data, use it, then delete it.
-            await File.WriteAllTextAsync("files", $"file '{Path.GetFileName(args[0])}'" + "\n" +
-                                                   "file data.bin");
+            await File.WriteAllTextAsync("files", $"file '{Path.GetFileName(args[0])}'" + $"\nfile data.bin");
 
             var process = new Process
             {
@@ -35,12 +32,7 @@ namespace DiscordCrasherVideoGenerator
                     CreateNoWindow = true, // For hiding the window that's going to be created.
                     WindowStyle = ProcessWindowStyle.Hidden, // For hiding the window that's going to be created.
                     FileName = "ffmpeg.exe",
-                    Arguments = "-loglevel quiet " // -loglevel quiet stop FFmpeg to print stuff to the console. "-c copy"
-                              + "-f concat " // Concat demuxer.
-                              + "-i files " // The file that contains input file names.
-                              + "-c copy " // Equals to "-c:v copy -c:a copy". It copies the codecs from the input files.
-                              + "-y " // Overwrites the output file, if it exists.
-                              + outputFileName
+                    Arguments = $"-loglevel quiet -f concat -i files -c copy -y {outputFileName}"
                 }
             };
 
